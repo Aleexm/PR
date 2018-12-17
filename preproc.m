5d563e26b1311705d71342e5784e7c5c02ff5cf7
simionAndrei <andrei.simion.c@gmail.com>
2018-12-17 15:40:31 +0100

    Add preprocessing first try

--- preproc.m
+++ preproc.m
@@ -1,17 +1,31 @@
-%% original data orig.jpg
-a = prnist([0:9], [1:40:1000]
+%% original data
+digits = prnist([0:9], [1:40:1000])
 
 %% put objects in a square box with aspect ratio 1
-%% add rows/columns to make images square step1.jpg
-obj1 = im_box(a, 0, 1)
+%% add rows/columns to make images square
+pdigits = im_box(digits, 0, 1)
 
-%%  rotate images to same orientation step2.jpg
-obj2 = im_rotate(obj1)
+%%  rotate images to same orientation
+%%obj2 = im_rotate(obj1)
 
-%% resize images to 128 x 128 pixels step3.jpg
-obj3 = im_resize(obj2, [128, 128])
+%% resize images to 128 x 128 pixels
+pdigits = im_resize(pdigits, [128, 128])
 
 %% create empty box around objects
-%% add rows/columns and keep image square step4.jpg
-obj4 = im_box(obj3, 1, 0)
+%% add rows/columns and keep image square
+pdigits = im_box(pdigits, 1, 0)
 
+%% iterate over prdatafile, convert to images
+%% close images and remove noise
+for i = 1:250
+    im = data2im(pdigits(i));
+    %% commented in order to don't generate 250x2 images
+    %% imwrite(im, sprintf('imgs/%s_%d.jpg',"orig",i));
+    se = strel('disk',2);
+    im = imclose(im, se);
+    im = imerode(im,se);
+    im = imdilate(im,se);
+    %% commented same reasoing as above
+    %% imwrite(im, sprintf('imgs/%s_%d.jpg',"proc",i));
+    p_imgs{i} = im;
+end
