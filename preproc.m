fd0ce3428a8ad14a4a572f6337961fcc5a6e1ea3
Alex <a.v.mandersloot@students.uu.nl>
2019-01-08 16:41:17 +0100

    Added Fisher's Criterion

--- preproc.m
+++ preproc.m
@@ -5,7 +5,8 @@
 
 %% Create your dataset for testing here
 % projectionDataset = createProjectionDataset(cleanedImages, labels);
-% profileDataset = createProfileDataset(cleanedImaged,labels);
+% profileDataset = createProfileDataset(cleanedImages,labels, 2);
+fisherDataset = selectFisherFeatures(projectionDataset,4);
 % fourierDataset = createFourierDataset(cleanedImages,labels);
 % 
 % %% Test classifiers
@@ -13,6 +14,77 @@
 % % use prcrossval(pdigits,[], 10) to perform 10-fold-cross validation to estimate test error
 % 
 % [error, errorPerClass] = prcrossval(projectionDataset, svc, 5);
+
+%% Compute Fisher's Criteria Feature Selection. Call with the feature dataset
+%% as well as the k-largest features you want to select.
+function fisherDataset = selectFisherFeatures(featDataset, k)
+    numOfFeatures = size(featDataset,2); %amount of features
+    numOfObjects = size(featDataset,1); %amount of objects
+    allFisherValues = zeros(numOfFeatures,1); %init array
+    priors = getprior(featDataset,0); %get class priors
+    for featIndex = 1:numOfFeatures %For each feature do
+        %Array containing feature variance according to each class
+        currentFeatureVariancePerClass = zeros(10,1); 
+        % I'm sorry
+        class1feats = [];
+        class2feats = [];
+        class3feats = [];
+        class4feats = [];
+        class5feats = [];
+        class6feats = [];
+        class7feats = [];
+        class8feats = [];
+        class9feats = [];
+        class10feats = [];
+        for objectIndex = 1:numOfObjects % Loop over all objects for this feat
+            label = featDataset.nlab(objectIndex); %get label
+            if label == 1
+                class1feats = [class1feats,featDataset.data(objectIndex,featIndex)];
+            elseif label == 2
+                class2feats = [class2feats,featDataset.data(objectIndex,featIndex)];
+            elseif label == 3
+                class3feats = [class3feats,featDataset.data(objectIndex,featIndex)];
+            elseif label == 4
+                class4feats = [class4feats,featDataset.data(objectIndex,featIndex)];
+            elseif label == 5
+                class5feats = [class5feats,featDataset.data(objectIndex,featIndex)];
+            elseif label == 6
+                class6feats = [class6feats,featDataset.data(objectIndex,featIndex)];                
+            elseif label == 7
+                class7feats = [class7feats,featDataset.data(objectIndex,featIndex)];
+            elseif label == 8
+                class8feats = [class8feats,featDataset.data(objectIndex,featIndex)];
+            elseif label == 9
+                class9feats = [class9feats,featDataset.data(objectIndex,featIndex)];
+            else
+                class10feats = [class10feats,featDataset.data(objectIndex,featIndex)];
+            end
+        end
+        currentFeatureVariancePerClass(1) = var(class1feats);
+        currentFeatureVariancePerClass(2) = var(class2feats);
+        currentFeatureVariancePerClass(3) = var(class3feats);
+        currentFeatureVariancePerClass(4) = var(class4feats);
+        currentFeatureVariancePerClass(5) = var(class5feats);
+        currentFeatureVariancePerClass(6) = var(class6feats);
+        currentFeatureVariancePerClass(7) = var(class7feats);
+        currentFeatureVariancePerClass(8) = var(class8feats);
+        currentFeatureVariancePerClass(9) = var(class9feats);
+        currentFeatureVariancePerClass(10) = var(class10feats);
+        
+        %compute Fisher's value for this feature
+        sum = 0;
+        for classIndex = 1:10
+            sum = sum + priors(classIndex) * ...
+                currentFeatureVariancePerClass(classIndex);
+        end
+        allFisherValues(featIndex) = 1/sum;
+    end
+    % Get index of maximum fishervalues
+    [fisherValues,index] = maxk(allFisherValues,k);
+    %Subset featuredataset by selecting only top k features
+    % (params: A, object subset, feature subset, some other random thing)
+    fisherDataset = seldat(featDataset,[],index,[]);
+end
 
 %% Create General Fourier Descriptors (does not work correctly yet)
 function fourierDataset = createFourierDataset(cleanedImages, labels)
