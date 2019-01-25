c0fb8f8656199527653f963591885a4d01956489
simionAndrei <andrei.simion.c@gmail.com>
2019-01-25 10:32:11 +0100

    Add scenario1 final classifier results

--- scenario1_final_test.m
+++ scenario1_final_test.m
@@ -4,12 +4,13 @@ final_models = {scalem([], 'variance') * pcam([], 0.95) * parzenc, ...
     scalem([], 'variance') * pcam([], 0.7) * parzenc, ...
     scalem([], 'variance') * pcam([], 0.6) * parzenc};
 
-resultsHeader = ["samples_per_class", "1NN", "95PCA_1NN", "90PCA_1NN", ...
-    "80PCA_1NN", "70PCA_1NN", "60PCA_1NN"]; 
+resultsHeader = ["samples_per_class", "95PCA_PARZEN", "90PCA_PARZEN", ...
+    "80PCA_PARZEN", "70PCA_PARZEN", "60PCA_PARZEN"]; 
 cell2csv('scenario1_results.csv', resultsHeader);
 for samplesPerClass=200:25:400
     nistDatafile = prnist([0:9], [1:samplesPerClass]);
     hogDataset = my_rep(nistDatafile);
+    hogDataset = hogDataset(randperm(size(hogDataset, 1)), :);
     crt_result = [samplesPerClass];
     for modelIndex = 1: size(final_models, 2)
         untrainedModel = final_models(modelIndex);
